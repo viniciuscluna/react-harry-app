ad10075d4426b4abb00f8ad8963916fe43fb4c08
Vinicius Luna <viniciuscluna@gmail.com>
2022-10-24 22:57:39 -0300

    Added dark style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 
 import { Outlet } from "react-router-dom";
+import { ThemeContext } from './contexts/theme-context';
 import Header from "./components/Header"
+import { useState } from "react";
 
 const App = () => {
+
+  const isBrowserDefaultDark = () => window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  const getDefaultTheme = (): string => {
+    const localStorageTheme = localStorage.getItem('default-theme');
+    const browserDefault = isBrowserDefaultDark() ? 'dark' : 'light';
+    return localStorageTheme || browserDefault;
+  };
+
+  const [theme, setTheme] = useState(getDefaultTheme());
+
   return (
-    <>
-      <Header />
-      <Outlet />
-    </>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`theme-${theme}`}>
+        <Header />
+        <div className="container-fluid content-body">
+          <Outlet />
+        </div>
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,49 @@
+import { useContext } from "react";
 import { NavLink, Link } from "react-router-dom";
-export default () => (
-    <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-            <Link className="navbar-brand" to={'/'}>Harry Potter App</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} to={`/Personagens`}>Personagens</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} to={`/Magias`}>Magias</NavLink>
-                    </li>
-                </ul>
+import { ThemeContext } from "../contexts/theme-context";
+import './Header.scss';
+
+export default () => {
+    const { theme, setTheme } = useContext(ThemeContext);
+  
+
+    const handleThemeChange = () => {
+        const isCurrentDark = theme === 'dark';
+        setTheme(isCurrentDark ? 'light' : 'dark');
+        localStorage.setItem('theme', isCurrentDark ? 'light' : 'dark');
+    };
+
+    return (
+        <nav className={`navbar navbar-expand-lg`}>
+            <div className="container-fluid">
+                <Link className="navbar-brand" to={'/'}>Harry Potter App</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} to={`/Personagens`}>Personagens</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} to={`/Magias`}>Magias</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <div className="toggle-btn-section">
+                    <div className={`toggle-checkbox m-vertical-auto`}>
+                        <input
+                            className="toggle-btn__input"
+                            type="checkbox"
+                            name="checkbox"
+                            onChange={handleThemeChange}
+                            checked={theme === 'light'}
+                        />
+                        <button type="button" className={`toggle-btn__input-label`} onClick={handleThemeChange}></button>
+                    </div>
+                </div>
             </div>
-        </div>
-    </nav>
-)
+        </nav>
+    )
+}
+
